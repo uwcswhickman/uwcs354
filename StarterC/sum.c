bd6283b30fc0fcc84b83541d67b4ec9764f620e6
Will Hickman <whickman@epic.com>
2019-01-28 19:44:25 -0600

    change name

---
+++ StarterC/sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	int a = 30;
+	int b = 70;
+	int c = a + b;
+	printf("c (dec) = %d\n", c);
+	printf("c (oct) = 0%o\n", c);
+	printf("c (hex) = 0x%x\n\n", c);
+}
+
