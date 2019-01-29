6122de0a4d4e188bb0df5077f53ed759376f934c
Will Hickman <whickman@epic.com>
2019-01-28 18:49:02 -0600

    make it match class example

--- StarterC/Source.c
+++ StarterC/Source.c
@@ -2,7 +2,12 @@
 int main()
 {
 	// printf() displays the string inside quotation
-	printf("Hello, World!");
-	return 0;
+	/* printf("Hello, World!");
+	return 0; */
+
+	int a = 1;
+	int b = 2;
+	int c = a + b;
+	printf("%d", c);
 }
 
